52fa17ba59029e8d47993d47424d6357df1261dc
Alex <belohvostov.belohvostov@gmail.com>
2022-10-12 17:12:09 +0300

    init page

---
+++ pages/tracks/createTrack.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import MainLayout from "../../layouts/MainLayout/MainLayout";
+
+const createTrack = () => {
+  return (
+    <MainLayout>
+      <h1>Loading track..</h1>
+    </MainLayout>
+  );
+};
+
+export default createTrack;
